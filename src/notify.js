ad89a15ebe3805dbe8bef3fd8f3ad3304dbd7a88
gkiwi <bugkiwi@gmail.com>
2016-09-29 14:25:21 +0800

    fix multiline text bug;

--- src/notify.js
+++ src/notify.js
@@ -57,7 +57,7 @@ class Toast extends React.Component {
 
 		const contentStyle = {
 			cursor: 'pointer',
-			display: 'inline',
+			display: 'inline-block',
 			width: 'auto',
 			borderRadius: '0 0 4px 4px',
 			backgroundColor: 'white',
